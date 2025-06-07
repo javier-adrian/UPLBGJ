3e9fa17e27f6412b1435602178c29aef91d135b2
Adrian Javier <javier.adrian.kld@gmail.com>
2025-06-08 00:17:17 +0800

    finish level sequence helper

--- scripts/editor.gd
+++ scripts/editor.gd
@@ -3,9 +3,13 @@ extends Node2D
 var editing: bool = false
 var title: String = "Rhythm Hell"
 
+var fall_delay: float = 4.66
+
+var key_output = [[], [], [], []]
+
 var info: Dictionary = {
 	"Rhythm Hell": {
-		"timings":  "[[0, 2],[0.5, 2.5],[1, 3],[1.5, 3.5]]",
+		"timings":  "[[4.75],[5.25],[5.75],[6.25]]",
 		"music": load("res://RhythmHell.wav")
 	}
 }
@@ -36,13 +40,19 @@ func _ready():
 					key_name = "right"
 
 			for delay in key:
-				spawn_note(key_name, delay)
+				spawn_note(key_name, delay - fall_delay)
 			
 			index += 1
 
 func listener_press(key: String, index: int):
-	print(index)
+	# print(index, " " + str($Player.get_playback_position()))
+	# key_output[index].append($Player.get_playback_position() - fall_delay)
+	key_output[index].append($Player.get_playback_position())
 
 func spawn_note(key: String, delay: float):
 	await get_tree().create_timer(delay).timeout
 	Manager.spawn_note.emit(key)
+
+
+func _on_player_finished() -> void:
+	print(key_output)
