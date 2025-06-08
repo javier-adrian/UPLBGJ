479b83835e46a4753563182ab32539473b9d7cb5
xITmasterx <elijaheumags@gmail.com>
2025-06-08 22:07:36 +0800

    reconnected the start menu buttons

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,4 +2,31 @@ extends Node2D
 
 func _input(event):
 	if Input.is_action_just_pressed("producer"):
+<<<<<<< Updated upstream
 		get_tree().change_scene_to_file("res://scenes/producer/producer.tscn")
+=======
+		get_tree().change_scene_to_file("res://scenes/producer/producer.tscn")
+
+func _on_start_menu_play() -> void:
+	$gameplay_background/AnimationPlayer.play("zoom_out")
+	$StartMenu/AnimationPlayer.play("play")
+	await get_tree().create_timer(2).timeout
+	# print(get_children())
+	# $background.queue_free()
+	var level_instance = level1.instantiate()
+	get_tree().root.call_deferred("add_child", level_instance)
+
+func _on_start_menu_about() -> void:
+	print("Main received About signal - changing scene")
+	get_tree().change_scene_to_file("res://scenes/About/About.tscn")
+
+func _on_start_menu_how_to_play() -> void:
+	print("Main received How to Play signal - changing scene")
+	get_tree().change_scene_to_file("res://scenes/HowToPlay/HowToPlay.tscn")
+
+func _on_start_menu_credits() -> void:
+	print("Main received Credits signal - changing scene")
+	get_tree().change_scene_to_file("res://scenes/Credits/Credits.tscn")
+
+	
+>>>>>>> Stashed changes
---
+++ scripts/start_menu.gd
@@ -0,0 +1,50 @@
+extends Control
+
+signal play()
+signal how_to_play()
+signal about()
+signal credits()
+
+func _on_play_button_down() -> void:
+	# First emit the signal for the main scene to handle
+	play.emit()
+	
+# 	# If we're not in the main scene or the signal isn't connected,
+# 	# directly change to the level scene after a short delay
+# 	var timer = get_tree().create_timer(0.5)
+# 	await timer.timeout
+	
+# 	# Check if we're still in the same scene (signal wasn't handled)
+# 	if is_instance_valid(self) and is_inside_tree():
+# 		get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+
+
+func _on_quit_button_down() -> void:
+	get_tree().quit()
+
+
+func _on_how_to_play_button_down() -> void:
+	print("How to Play button clicked - emitting signal")
+	how_to_play.emit()
+
+
+func _on_about_button_down() -> void:
+	print("About button clicked - emitting signal")
+	about.emit()
+
+
+func _on_credits_button_down() -> void:
+	print("Credits button clicked - emitting signal")
+	credits.emit()
+
+
+# Alternative direct method if signal approach doesn't work
+func play_game_direct() -> void:
+	pass
+	# get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+
+# You can call this from the editor by connecting the pressed signal
+# to this method instead of button_down to _on_play_button_down
+func _on_play_pressed() -> void:
+	pass
+	# play_game_direct()
