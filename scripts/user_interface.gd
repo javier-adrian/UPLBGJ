d3cf629e7fdc17661a980ef9498425426ae5c436
xITmasterx <elijaheumags@gmail.com>
2025-06-08 15:29:16 +0800

    combo scoring implementation
    combo scoring implementation

--- scripts/user_interface.gd
+++ scripts/user_interface.gd
@@ -2,6 +2,8 @@ extends Control
 
 var score: int = 0
 var combo: int = 0
+var combo_multiplier: float = 1.0
+var combo_threshold: int = 5  # Combo needed for multiplier
 
 func _ready():
 	Manager.increment_score.connect(increment_score)
@@ -9,13 +11,26 @@ func _ready():
 	Manager.reset_combo.connect(reset_combo)
 
 func increment_score(n: int):
-	score += n
+	var points = n
+	if combo >= combo_threshold:
+		points = int(n*combo_multiplier)
+	score += points
 	%score.text = str(score)
 
 
 func increment_combo():
 	combo += 1
 	%combo.text = str(combo)
+	
+	if combo >= combo_threshold:
+		%combo.text = str(combo) + " (x" + str(combo_multiplier) + ")"
+	else:
+		%combo.text = str(combo)
+	
+	if combo == combo_threshold:
+		combo_multiplier = 1.5
+	elif combo > combo_threshold:
+		combo_multiplier = min(1.5 + (combo - combo_threshold) * 0.1, 3.0)
 
 func reset_combo():
 	combo = 0
