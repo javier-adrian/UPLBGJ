f80d7f35c9503f0bffa3b18aeb60f17826923f26
Adrian Javier <javier.adrian.kld@gmail.com>
2025-06-08 18:45:38 +0800

    recover lost changes

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,19 +1,22 @@
 extends Control
 
 signal play()
+signal how_to_play()
+signal about()
+signal credits()
 
 func _on_play_button_down() -> void:
 	# First emit the signal for the main scene to handle
 	play.emit()
 	
-	# If we're not in the main scene or the signal isn't connected,
-	# directly change to the level scene after a short delay
-	var timer = get_tree().create_timer(0.5)
-	await timer.timeout
+# 	# If we're not in the main scene or the signal isn't connected,
+# 	# directly change to the level scene after a short delay
+# 	var timer = get_tree().create_timer(0.5)
+# 	await timer.timeout
 	
-	# Check if we're still in the same scene (signal wasn't handled)
-	if is_instance_valid(self) and is_inside_tree():
-		get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+# 	# Check if we're still in the same scene (signal wasn't handled)
+# 	if is_instance_valid(self) and is_inside_tree():
+# 		get_tree().change_scene_to_file("res://scenes/level/level.tscn")
 
 
 func _on_quit_button_down() -> void:
@@ -34,9 +37,11 @@ func _on_credits_button_down() -> void:
 
 # Alternative direct method if signal approach doesn't work
 func play_game_direct() -> void:
-	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+	pass
+	# get_tree().change_scene_to_file("res://scenes/level/level.tscn")
 
 # You can call this from the editor by connecting the pressed signal
 # to this method instead of button_down to _on_play_button_down
 func _on_play_pressed() -> void:
-	play_game_direct()
+	pass
+	# play_game_direct()
