3684d7c7091e7e45ba1aa2696e89a38c09522be5
xITmasterx <elijaheumags@gmail.com>
2025-06-08 17:51:05 +0800

    Additional scripting and logic fallbacks for start screen

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,36 @@
 extends Node2D
 
+func _ready():
+    # Make sure the StartMenu is connected to this script
+    if $StartMenu.is_connected("play", Callable(self, "_on_start_menu_play")):
+        print("StartMenu play signal is connected")
+    else:
+        print("StartMenu play signal is NOT connected")
+        $StartMenu.connect("play", Callable(self, "_on_start_menu_play"))
+
 func _input(event):
-	if Input.is_action_just_pressed("producer"):
-		get_tree().change_scene_to_file("res://scenes/producer/producer.tscn")
+    if Input.is_action_just_pressed("producer"):
+        get_tree().change_scene_to_file("res://scenes/producer/producer.tscn")
 
 func _on_start_menu_play() -> void:
-	$background/AnimationPlayer.play("zoom_out")
-	$StartMenu/AnimationPlayer.play("play")
+    print("Play signal received")
+    
+    # Check if the animation player exists
+    if $background.has_node("AnimationPlayer"):
+        $background/AnimationPlayer.play("zoom_out")
+    else:
+        print("Background AnimationPlayer not found")
+    
+    if $StartMenu.has_node("AnimationPlayer"):
+        $StartMenu/AnimationPlayer.play("play")
+        
+        # Wait for animation to finish before changing scene
+        await $StartMenu/AnimationPlayer.animation_finished
+        print("Animation finished, changing to level scene")
+        get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+    else:
+        print("StartMenu AnimationPlayer not found")
+        # If no animation player, just change scene directly
+        get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+
+
--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -3,8 +3,40 @@ extends Control
 signal play()
 
 func _on_play_button_down() -> void:
+	# First emit the signal for the main scene to handle
 	play.emit()
+	
+	# If we're not in the main scene or the signal isn't connected,
+	# directly change to the level scene after a short delay
+	var timer = get_tree().create_timer(0.5)
+	await timer.timeout
+	
+	# Check if we're still in the same scene (signal wasn't handled)
+	if is_instance_valid(self) and is_inside_tree():
+		get_tree().change_scene_to_file("res://scenes/level/level.tscn")
 
 
 func _on_quit_button_down() -> void:
 	get_tree().quit()
+
+
+func _on_how_to_play_button_down() -> void:
+	get_tree().change_scene_to_file("res://scenes/HowToPlay/HowToPlay.tscn")
+
+
+func _on_about_button_down() -> void:
+	get_tree().change_scene_to_file("res://scenes/About/About.tscn")
+
+
+func _on_credits_button_down() -> void:
+	get_tree().change_scene_to_file("res://scenes/Credits/Credits.tscn")
+
+
+# Alternative direct method if signal approach doesn't work
+func play_game_direct() -> void:
+	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+
+# You can call this from the editor by connecting the pressed signal
+# to this method instead of button_down to _on_play_button_down
+func _on_play_pressed() -> void:
+	play_game_direct()
